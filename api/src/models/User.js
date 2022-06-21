d27a16e7f8243b60db73bbf36c4e9a5242e1c779
santiserena <santiserena@outlook.com.ar>
2022-06-21 12:48:07 -0300

    primera version de modelos db

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -1,4 +1,4 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes, STRING } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
@@ -6,6 +6,37 @@ module.exports = (sequelize) => {
   // defino el modelo
   
   sequelize.define('user', {
+    
+    mail: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      primaryKey: true,
+    },
+
+    contraseña: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    usuario: {
+      type: DataTypes.STRING,
+      allowNull:false,
+    },
+
+    direccion: {
+      type: DataTypes.STRING,
+      allowNull:false,
+    },  
+
+    nombre: {
+      type: DataTypes.STRING,
+      allowNull:false,
+    },  
+
+    apellido: {
+      type: DataTypes.STRING,
+      allowNull:false,
+    },  
     
   });
 

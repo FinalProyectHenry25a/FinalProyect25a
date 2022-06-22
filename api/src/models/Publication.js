4e3694f4de535008ab775194f20af7d655bb6b48
andrescn2020 <andrescn_2010@hotmail.com>
2022-06-22 14:13:24 -0300

    Agregado de stock al modelo y db, Ruta put de stock y get del detalle del celular

--- api/src/models/Publication.js
+++ api/src/models/Publication.js
@@ -6,93 +6,98 @@ module.exports = (sequelize) => {
       type: DataTypes.UUID,
       defaultValue: DataTypes.UUIDV4,
       allowNull: false,
-      primaryKey: true,
+      primaryKey: true
     },
 
     brand: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: false
     },
 
     releaseDate: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: false
     },
 
     model: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: false
     },
 
     price: {
       type: DataTypes.FLOAT,
-      allowNull: false,
+      allowNull: false
     },
 
     rating: {
       type: DataTypes.INTEGER,
-      allowNull: true,
+      allowNull: true
     },
 
     images: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: false
     },
 
     color: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: false
     },
 
     processor: {
       type: DataTypes.STRING,
-      allowNull: true,
+      allowNull: true
     },
 
     ram: {
       type: DataTypes.ENUM("4Gb", "6Gb", "8Gb", "12Gb"),
-      allowNull: true,
+      allowNull: true
     },
 
     rom: {
       type: DataTypes.ENUM("32Gb", "64Gb", "128Gb", "256Gb"),
-      allowNull: true,
+      allowNull: true
     },
 
     network: {
       type: DataTypes.ENUM("3G", "4G", "5G"),
-      allowNull: true,
+      allowNull: true
     },
 
     batery: {
       type: DataTypes.INTEGER,
-      allowNull: true,
+      allowNull: true
     },
 
     frontal_cam: {
       type: DataTypes.INTEGER,
-      allowNull: false,
+      allowNull: false
     },
 
     main_cam: {
       type: DataTypes.INTEGER,
-      allowNull: false,
+      allowNull: false
     },
 
     inches: {
       type: DataTypes.FLOAT,
-      allowNull: true,
+      allowNull: true
     },
 
     screen: {
       type: DataTypes.STRING,
-      allowNull: true,
+      allowNull: true
     },
 
     resolution: {
       type: DataTypes.STRING,
-      allowNull: true,
+      allowNull: true
     },
+
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
 
   });
 };

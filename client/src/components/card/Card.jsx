415d0625b36ce1b02a4354216a98545f5c5def80
MaxiBlanc <maxiblanc240801@gmail.com>
2022-06-22 16:04:52 -0300

    home_renderize

--- client/src/components/card/Card.jsx
+++ client/src/components/card/Card.jsx
@@ -5,11 +5,12 @@ export default function Card(props) {
   console.log(props);
   return (
       <div className={style.card}>
-      <h3>{props.name}</h3>
-        <div>
-        <img src={props.imagenes} className={style.image} alt="out" />
+      <h3>{props.brand}</h3>
+      <h3>{props.model}</h3>
+        <div>  
+        <img src={props.images} className={style.image} alt="out" />
       </div>
-        <h4>{props.precio}</h4>
+        <h4>{props.price}</h4>
         <button type="submit">Agregar al carrito</button>
     </div>
   );

f06ee7add0a62c8deaa310a5b08a62aa8e566002
Franco Tejada <frante.2717@gmail.com>
2022-06-22 20:59:24 -0300

    ruta creacion de usuarios

--- api/src/routes/userCreator.js
+++ api/src/routes/userCreator.js
@@ -1,5 +1,22 @@
-const Router = require ('express')
+const Router = require ('express');
+const {User } = require('../db');
 
 const router = Router ();
+
+router.post('/', async(req, res) => {
+    const { email, password, username, address, firstname, lastname } = req.body;
+
+    await User.create({
+        email,
+        password,
+        username,
+        address,
+        firstname,
+        lastname
+    })
+
+    res.status(200).send("successfully created");
+})
+
 
 module.exports = router;

1da49a575397a11ace6f4ea976fe5781bdea27e3
Franco Tejada <frante.2717@gmail.com>
2022-06-23 23:26:13 -0300

    arreglando rutas cart y favourites y eliminando login, register y addCart

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,42 +1,36 @@
-const { Router } = require('express');
-const publication = require('./publication.js');
-const filtersAndOrders  = require ('./filtersAndOrders');
-const postCreator = require ('./postCreator');
-const userCreator = require ('./userCreator');
-const deletePosts = require('./deletePost');
-const  register  = require("./register")
-const  login  = require("./login")
-const users = require("./users");
-const addCart = require('./addCart');
+const { Router } = require("express");
+const publication = require("./publication.js");
+const filtersAndOrders = require("./filtersAndOrders");
+const postCreator = require("./postCreator");
+const userCreator = require("./userCreator");
+const deletePosts = require("./deletePost");
+const favourites = require("./favourites");
+const cart = require("./addCart");
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-router.use("/login", login)
-
-router.use("/register", register)
 
 //trae todas la publicaciones o por query por modelo
-router.use('/home', publication)
+router.use("/home", publication);
 
 //crea una publicacion
-router.use('/postCreator', postCreator)
+router.use("/postCreator", postCreator);
 
-//crea un usuario 
-router.use('/userCreator', userCreator)
+//crea un usuario
+router.use("/userCreator", userCreator);
 
 //filtrados
-router.use('/filtersAndOrders', filtersAndOrders)
+router.use("/filtersAndOrders", filtersAndOrders);
 
 //elimina un posteo
-router.use('/delete', deletePosts)
+router.use("/delete", deletePosts);
 
 //agregar al carrito
-router.use('/addCart', addCart);
+router.use("/cart", cart);
 
 //get de usuarios
-router.use('/users', users)
-
+router.use("/favourites", favourites);
 
 module.exports = router;

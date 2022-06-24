aceeb626f509535f8da6f1e30e4b4d16bdacaa92
andrescn2020 <andrescn_2010@hotmail.com>
2022-06-24 16:18:16 -0300

    Bcrypt al userCreator

--- api/src/routes/userCreator.js
+++ api/src/routes/userCreator.js
@@ -1,18 +1,24 @@
 const Router = require ('express');
-const {User } = require('../db');
+const { User } = require('../db');
+const bcrypt = require("bcrypt");
 
 const router = Router ();
 
 router.post('/', async(req, res) => {
+
     const { email, password, username, address, firstname, lastname } = req.body;
 
+    const hashedPassword = await bcrypt.hash(password, 10)
+
     await User.create({
-        email,
-        password,
-        username,
-        address,
-        firstname,
-        lastname
+
+        email: email,
+        password: hashedPassword,
+        username: username,
+        address: address,
+        firstname: firstname,
+        lastname: lastname
+        
     })
 
     res.status(200).send("successfully created");

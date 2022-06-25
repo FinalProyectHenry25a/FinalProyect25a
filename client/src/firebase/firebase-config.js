51c0dc91ee8206f213bfe8c6cc59c44ab7182fb9
andrescn2020 <andrescn_2010@hotmail.com>
2022-06-25 15:51:56 -0300

    Nueva cuenta en firebase

--- client/src/firebase/firebase-config.js
+++ client/src/firebase/firebase-config.js
@@ -9,18 +9,19 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
+
 const firebaseConfig = {
 
-  
-  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
-  appId: process.env.REACT_APP_FIREBASE_APPID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID
-  
+  apiKey: "AIzaSyD9uveHHol4bMl3DviVhDgoXe48rIuHPkA",
+  authDomain: "e-commerce-dcfb1.firebaseapp.com",
+  projectId: "e-commerce-dcfb1",
+  storageBucket: "e-commerce-dcfb1.appspot.com",
+  messagingSenderId: "16952118469",
+  appId: "1:16952118469:web:5cb26f457afcded57d74ac"
+
 };
+
+console.log(firebaseConfig.apiKey);
 
 
 // Initialize Firebase

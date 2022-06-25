e96c7c17685d66ebb148ad30040b3146b940293a
Franco Tejada <frante.2717@gmail.com>
2022-06-24 22:12:18 -0300

    eliminacion y adhesion a la ruta admin

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,9 +1,7 @@
 const { Router } = require('express');
 const publication = require('./publication.js');
 const filtersAndOrders  = require ('./filtersAndOrders');
-const postCreator = require ('./postCreator');
 const userCreator = require ('./userCreator');
-const deletePosts = require('./deletePost');
 const favourites = require("./favourites");
 const cart = require("./cart");
 const admin = require("./admin");
@@ -16,23 +14,19 @@ const router = Router();
 //TRAE TODAS LAS PUBLICACIONES O POR QUERY O POR MODELOS
 router.use('/home', publication)
 
-//CREAR PUBLICACIONES
-router.use('/postCreator', postCreator)
-
 //CREAR USUARIOS
 router.use('/userCreator', userCreator)
 
 //FILTRADOS
 router.use('/filtersAndOrders', filtersAndOrders)
 
-//elimina un posteo
-router.use('/delete', deletePosts)
-
+//RUTA FAVORITOS
 router.use("/favourites", favourites);
 
+//RUTA CARRITO
 router.use("/cart", cart);
 
-// RUTA EXCLUSIVA ADMINS
+//RUTA EXCLUSIVA ADMINS
 router.use('/admin', admin)
 
 module.exports = router;

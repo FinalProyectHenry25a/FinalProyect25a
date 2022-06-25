a14d4845581e35f68ec45c3351d9678dc28c7b70
Franco Tejada <frante.2717@gmail.com>
2022-06-24 22:12:18 -0300

    eliminacion y adhesion a la ruta admin

--- api/src/routes/admin.js
+++ api/src/routes/admin.js
@@ -69,5 +69,27 @@ router.post("/post", async (req, res) => {
     res.status(404).send(error);
   }
 });
+/*
+para probar y como ejemplo del body:
+{
+    "brand": "santi",
+    "releaseDate": "alguna fecha",
+    "model": "ssdcs",
+    "price": 250,
+    "rating": 5,
+    "images": "slksdmlcsmk",
+    "color": "rojo",
+    "processor": "sdsfds",
+    "ram":"4Gb",
+    "rom":"32Gb",
+    "network":"3G",
+    "batery":6,
+    "frontal_cam":5,
+    "main_cam":7,
+    "inches": 10,
+    "screen": 11,
+    "resolution":"sdsdf",
+    "stock":5
+} */
 
 module.exports = router;

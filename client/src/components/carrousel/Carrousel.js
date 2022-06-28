dd9c9f96c5d20c779857febab013aa04294ca78d
Manuel Ricardes <manuelalbertoricardes@gmail.com>
2022-06-28 20:05:45 -0300

    errores solucionados y consola limpia

--- client/src/components/carrousel/Carrousel.js
+++ client/src/components/carrousel/Carrousel.js
@@ -50,64 +50,64 @@ const Carrousel = () => {
   //   }, 5000)
   // }, [])
   return (
-  <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active" data-bs-interval="2000">
+  <div id="carouselExampleInterval" className="carousel slide" data-bs-ride="carousel">
+  <div className="carousel-inner">
+    <div className="carousel-item active" data-bs-interval="2000">
       <img src={iphone2} style={{display: "block", width: 100 + "%", height: 500 + "px"}} alt="..."/>
     </div>
-    <div class="carousel-item" data-bs-interval="2000">
+    <div className="carousel-item" data-bs-interval="2000">
       <img src={moto} style={{display: "block", width: 100 + "%", height: 500 + "px"}} alt="..."/>
     </div>
-    <div class="carousel-item" data-bs-interval="2000">
+    <div className="carousel-item" data-bs-interval="2000">
       <img src={huawei}  style={{display: "block", width: 100 + "%", height: 500 + "px"}} alt="..."/>
     </div>
-    <div class="carousel-item" data-bs-interval="2000">
+    <div className="carousel-item" data-bs-interval="2000">
       <img src={samsung} style={{display: "block", width: 100 + "%", height: 500 + "px"}} alt="..."/>
     </div>
-    <div class="carousel-item">
+    <div className="carousel-item">
       <img src={note} style={{display: "block", width: 100 + "%", height: 500 + "px"}} alt="..."/>
     </div>
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Previous</span>
   </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Next</span>
   </button>
 </div>
   );
 };
 {
-  /*     <main className={styles.mainContainer}>
-      <div className={styles.principalContainer}>
-        <div className={styles.slideContainer} ref={slideShow}>
-          <div className={styles.slide}>
-            <img className={style.image} src={huawei} alt="huawei" />
+  /*     <main classNameName={styles.mainContainer}>
+      <div classNameName={styles.principalContainer}>
+        <div classNameName={styles.slideContainer} ref={slideShow}>
+          <div classNameName={styles.slide}>
+            <img classNameName={style.image} src={huawei} alt="huawei" />
             
           </div>
 
-          <div className={styles.slide}>
-            <img className={style.image} src={samsung} alt="samsung" />
+          <div classNameName={styles.slide}>
+            <img classNameName={style.image} src={samsung} alt="samsung" />
             
           </div>
-          <div className={styles.slide}>
-            <img className={style.image} src={iphone} alt="iphone" />
+          <div classNameName={styles.slide}>
+            <img classNameName={style.image} src={iphone} alt="iphone" />
             
           </div>
-          <div className={styles.slide}>
-            <img className={style.image} src={lg} alt="lg" />
+          <div classNameName={styles.slide}>
+            <img classNameName={style.image} src={lg} alt="lg" />
             
           </div>
-          <div className={styles.slide}>
-            <img className={style.image} src={moto} alt="moto" />
+          <div classNameName={styles.slide}>
+            <img classNameName={style.image} src={moto} alt="moto" />
             
           </div>
         </div>
-        <div className={styles.controlers}>
-          <button onClick={previous} className={styles.btnLeft} ><IconLeft /></button>
-          <button onClick={next} className={styles.btnRight}><IconRight /></button>
+        <div classNameName={styles.controlers}>
+          <button onClick={previous} classNameName={styles.btnLeft} ><IconLeft /></button>
+          <button onClick={next} classNameName={styles.btnRight}><IconRight /></button>
         </div>
       </div>
     </main> */

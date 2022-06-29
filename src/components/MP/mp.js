bf3aaaa8fff86ad3076cd48f01f8dafeaf192e24
andrescn2020 <andrescn_2010@hotmail.com>
2022-06-28 23:33:25 -0300

    pull

---
+++ src/components/MP/mp.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react'
+import Comprar from '../Comprar/Comprar'
+import axios from 'axios'
+
+function App() {
+  const [datos, setDatos] = useState("")
+
+  useEffect(()=>{
+    axios
+    .get("http://localhost:3001/mercadopago")
+    .then((data)=>{
+      setDatos(data.data)
+      console.info('Contenido de data:', data)
+    }).catch(err => console.error(err)) 
+  },[])
+
+
+  const productos = [
+    {title: "Producto 1", quantity: 5, price: 10.52},
+    {title: "Producto 2", quantity: 15, price: 100.52},
+    {title: "Producto 3", quantity: 6, price: 200}
+  ]
+  return (
+    <div className="App">
+      { !datos
+        ? <p>Aguarde un momento....</p> 
+        : <Comprar productos={productos} data={datos}/>
+      }
+    </div>
+  );
+}
+
+export default App;

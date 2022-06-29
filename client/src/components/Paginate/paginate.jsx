6798b3f06549cd29a966f5c3c0af1b112e16771a
santiserena <santiserena@outlook.com.ar>
2022-06-29 16:42:43 -0300

    paginado anterior-siguiente

--- client/src/components/Paginate/paginate.jsx
+++ client/src/components/Paginate/paginate.jsx
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./paginate.css";
 
 export default function Paginado({ phonesPerPage, allPhones, paginado }) {
+
+  const [currentPage, setCurrentPage] = useState(1)
   const pageNumbers = [];
+
 
   for (let i = 0; i < Math.ceil(allPhones / phonesPerPage); i++) {
     pageNumbers.push(i + 1);
+  }
+
+  function nextPage() {
+    if (!(currentPage >= Math.ceil(allPhones / phonesPerPage))) {
+      paginado(currentPage + 1);
+      setCurrentPage(currentPage + 1);
+    }
+  }
+
+  function previousPage() {
+    if (currentPage - 1 > 0) {
+      paginado(currentPage - 1);
+      setCurrentPage(currentPage - 1);
+    }
+  }
+
+
+  function pag(number) {
+    paginado(number);
+    setCurrentPage(number);
   }
 
   return (
@@ -14,22 +37,20 @@ export default function Paginado({ phonesPerPage, allPhones, paginado }) {
         {pageNumbers &&
           pageNumbers.map((number) => (
             <li className="page-item" key={number}>
-              <button className="page-link" onClick={() => paginado(number)}>
+              <button className="page-link" onClick={() => pag(number)}>
                 {number}
               </button>
             </li>
           ))}
       </ul>
+
+      <ul className="pagination justify-content-center">
+        <button className="page-link" onClick={()=>previousPage()}>Anterior</button>
+        <label className="alert alert-light">{currentPage}/{Math.ceil(allPhones / phonesPerPage)}</label>
+        <button className="page-link" onClick={()=>nextPage()}>Siguiente</button>
+      </ul>        
+
     </nav>
   );
 }
-/*   <nav className="nav">
-            <ul className="paginado">
-                {pageNumbers &&
-                pageNumbers.map(number => (
-                    <li  className="li" key={number}>
-                    <button onClick={() => paginado(number)}>{number}</button>
-                    </li>
-                ))}
-            </ul>
-        </nav> */
+

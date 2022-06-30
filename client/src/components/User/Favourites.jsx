001b7f8998dc20601e5c82fd7887741b9052d145
Manuel Ricardes <manuelalbertoricardes@gmail.com>
2022-06-30 14:50:22 -0300

    limpiar filtros

--- client/src/components/User/Favourites.jsx
+++ client/src/components/User/Favourites.jsx
@@ -43,6 +43,7 @@ export default function Favourites() {
                   images={e.images}
                   price={e.price}
                   id={e.id}
+                  stock={e.stock}
                   />
                   
               </div>
--- client/src/components/card/Card.jsx
+++ client/src/components/card/Card.jsx
@@ -6,6 +6,7 @@ import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import { addToCart } from "../../Actions";
+import soldOut from "../../images/sold-out.png"
 
 export default function Card(props) {
 
@@ -40,7 +41,13 @@ export default function Card(props) {
 
   return (
     <div className="card" style={{width: 18 + 'rem', display: "inline-flex", flexFlow: "row wrap", justifyContent: "center"}} >
-      <img src={props.images} style={{height: 300 + "px" }} alt="..." />
+      <div style={{height: 300 + "px" }}>
+      {props.stock>0?(
+        <img src={props.images} style={{height: 300 + "px" }} alt="..." />
+      ):
+      <img src={soldOut} style={{height: 200 + "px" }} alt="..." />
+      }
+      </div>
       <div className="card-body">
         <h3 className="card-title">{props.brand}</h3>
         <h3>{props.model}</h3>
@@ -50,9 +57,13 @@ export default function Card(props) {
           <br />
         </div>
         {props.stock>0?(
+          <div>
         <Link to="#">
           <button className="btn btn-outline-dark, w-100" type="submit"  onClick={e => dispatch(addToCart(props.id))}>Agregar al carrito</button>
         </Link>
+        <p>Disponibles: {props.stock}</p>
+        </div>
+        
         ):<p>AGOTADO</p>}
         <br/>
         <br/>

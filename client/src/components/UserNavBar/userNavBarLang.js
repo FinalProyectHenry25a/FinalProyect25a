407b7f787de591839b29d0d917fda59a43ff3f46
santiserena <santiserena@outlook.com.ar>
2022-07-08 12:11:52 -0300

    import

--- client/src/components/UserNavBar/userNavBarLang.js
+++ client/src/components/UserNavBar/userNavBarLang.js
@@ -1,20 +1,9 @@
 export const userNavBarLang = {
-    es: {
-        'Miperfil':'Miperfillll',
-      'AgregarAlCarrito':'Agregar al carrito',
-      'AGOTADO':'AGOTADO',
-      'Detalle':'Detalle',
-      'Disponibles':'Disponibles',
-    },
-  
-    en: {
-        'Miperfil':'Miperfillll',
-      'AgregarAlCarrito':'Add to cart',
-      'AGOTADO':'SOLD OUT',
-      'Detalle':'Details',
-      'Disponibles':'Available',
-    },
-  };
-  
-  
-  
+  es: {
+    Miperfil: "Mi perfil",
+  },
+
+  en: {
+    Miperfil: "My profile",
+  },
+};

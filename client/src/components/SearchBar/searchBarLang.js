872417b809224047a95d28b97c51d5a5c686f45b
andrescn2020 <andrescn_2010@hotmail.com>
2022-07-13 16:31:36 -0300

    Stiling

--- client/src/components/SearchBar/searchBarLang.js
+++ client/src/components/SearchBar/searchBarLang.js
@@ -1,9 +1,11 @@
 export const searchBarLang = {
   es: {
     buscar: "Buscar",
+    buscaCelular: "Escriba aqui el celular que desea buscar..."
   },
 
   en: {
     buscar: "Find",
+    buscaCelular: "Write here the cell phone you want to search..."
   },
 };

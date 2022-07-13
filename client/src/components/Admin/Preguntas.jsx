3772ef25115d0ad09843e2021f5443104af2089e
santiserena <santiserena@outlook.com.ar>
2022-07-13 16:29:25 -0300

    estilos admin

--- client/src/components/Admin/Preguntas.jsx
+++ client/src/components/Admin/Preguntas.jsx
@@ -8,7 +8,7 @@ import { useHistory } from "react-router-dom";
 import {getAllUsers, getQuestions, getUser } from "../../Actions";
 import { auth } from "../../firebase/firebase-config";
 
-import style from "./../Admin/Admin.module.css"
+import style from "./Admin.module.css"
 
 export default function Preguntas(){
 const allQuestions = useSelector((state) => state.questions)
@@ -82,60 +82,90 @@ const eliminar = async (e)=>{
       window.location.reload()
 }
 
-return(
-    <div>
-          <Link to="/home">
-        <button className={style.btn}>Volver</button>
-      </Link>
-            <h1 className="d-flex justify-content-center">Preguntas</h1>
-         { allQuestions? allQuestions.map(e=>{      
-             
-            return(
-                <div className="row justify-content-center mt-4">
-                <div id={e.id} className="border rounded align-items-center justify-content-center w-75">
-                    <p>Usuario: {e.user_email}</p>
-                    <p>-{e.question}</p>
-                   
-                   {!e.answer?
-                   <div id={e.id} className="w-100 row">
-                   <input className="form-control w-50 me-3" onChange={(e) => handlerChange(e)} type="text" placeholder="responder..." />
-                    <button className="btn btn-secondary w-25" onClick={(e) => responder(e)}>responder</button>
-                    </div>
-                    :
-                    (
-                        <div id={e.id}>
-                    <p>-{e.answer}</p>
-                    <div className="d-flex justify-content-center">
-                    <button className="btn btn-secondary d-flex justify-content-center align-items-center" onClick={(e) => editar(e)}>borrar respuesta</button>
-                    </div>
-                    </div>
-                   )}
-                   <br />
-                   <div></div>
-                   <button className="d-flex justify-content-center btn btn-danger" onClick={(e) => eliminar(e)}>Eliminar pregunta</button>
-                   </div>
+return (
+  <div className={style.fondo}>
+    <Link to="/home">
+      <button className={style.btn}>Volver</button>
+    </Link>
+
+
+    <div className="d-flex justify-content-center">
+      <h1
+        style={{ background: "#4B657C" }}
+        className="col-6 row justify-content-center shadow py-2 px-4 rounded text-white"
+      >
+        Preguntas
+      </h1>
+    </div>
+
+    {allQuestions ? (
+      allQuestions.map((e) => {
+        return (
+          <div className="row justify-content-center mt-4">
+            <div
+              id={e.id}
+              className="border rounded align-items-center justify-content-center w-75"
+            >
+              <p>Usuario: {e.user_email}</p>
+              <p>-{e.question}</p>
+
+              {!e.answer ? (
+                <div id={e.id} className="w-100 row">
+                  <input
+                    className="form-control w-50 me-3"
+                    onChange={(e) => handlerChange(e)}
+                    type="text"
+                    placeholder="responder..."
+                  />
+                  <button
+                    className="btn btn-secondary w-25"
+                    onClick={(e) => responder(e)}
+                  >
+                    responder
+                  </button>
                 </div>
-            )
+              ) : (
+                <div id={e.id}>
+                  <p>-{e.answer}</p>
+                  <div className="d-flex justify-content-center">
+                    <button
+                      className="btn btn-secondary d-flex justify-content-center align-items-center"
+                      onClick={(e) => editar(e)}
+                    >
+                      borrar respuesta
+                    </button>
+                  </div>
+                </div>
+              )}
+              <br />
+              <div></div>
+              <button
+                className="d-flex justify-content-center btn btn-danger"
+                onClick={(e) => eliminar(e)}
+              >
+                Eliminar pregunta
+              </button>
+            </div>
+          </div>
+        );
         // quiero retornar TODAS las preguntas de TODOS los celulars
 
         //   e.map(el=>{
-        
+
         //          return(
         //              <div>
         //                 <p>lsa preguntas son: </p>
-                                                
+
         //                 {/* <p>{el.pregunta}</p> */}
-                                             
+
         //              </div>
         //          )
 
         //      })
-         
-            }):(
-            <p>no hay preguntas</p>
-          )
-}
-      
-        </div>
-    )
+      })
+    ) : (
+      <p>no hay preguntas</p>
+    )}
+  </div>
+);
 }
